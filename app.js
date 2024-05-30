747c585c1919401a0c3b2488451685327a2bfa11
“tanos0123” <lemayor01@hotmail.com>
2024-05-30 16:43:37 -0500

    "cambios"

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// Genera un número mágico aleatorio entre 0 y 99
 let numeroMagico = Math.floor(Math.random() * 100);
 
 // Selecciona los elementos del DOM
@@ -6,15 +5,12 @@ let input = document.querySelector("#numero-usuario");
 let boton = document.querySelector("#boton-adivinar");
 let mensaje = document.querySelector("#mensaje");
 
-// Imprime el número mágico en la consola (para depuración)
 console.log(numeroMagico);
 
 // Añade el evento click al botón
 boton.addEventListener("click", () => {
-  // Obtiene el valor del input y lo convierte a número
   const valorInput = parseInt(input.value);
   
-  // Verifica si el valor del input es igual al número mágico
   if (valorInput === numeroMagico) {
     
     mensaje.innerText = "¡Ganaste!";
